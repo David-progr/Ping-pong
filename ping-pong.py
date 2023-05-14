52a4f5c9ef368a6ed6f13cba55650cfb53fea1f8
David <128354618+David-progr@users.noreply.github.com>
2023-05-14 10:42:43 +0300

    Update ping-pong.py
    v2

--- ping-pong.py
+++ ping-pong.py
@@ -31,26 +31,27 @@ class Player(GameSprite):
     def update2(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 0:
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_heght - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 110:
             self.rect.y += self.speed
     def update1(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 0:
+        if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_heght - 80:
+        if keys[K_s] and self.rect.y < win_height - 110:
             self.rect.y += self.speed
-"""class Ball(GameSprite):
-    pass
-"""
-"""racketka1 = Player('', 0, 0, 5, 20, 80)
-racketka2 = Player('', win_width - 20, 0, 5, 20, 80)"""
+play1 = Player('racket.png', 20, 0, 5, 20, 100)
+play2 = Player('racket.png', win_width - 40, 0, 5, 20, 100)
+ball = GameSprite('ball.png', win_width / 2, win_height / 2, 0, 40, 40)
 game = True
 clock = time.Clock()
 FPS = 120
 finish = False
+
+speed_y = 2
+speed_x = 2
 
 while game:
     for e in event.get():
@@ -58,6 +59,24 @@ while game:
             game = False
     if finish != True:
         window.blit(bg, (0, 0))
+        play1.update1()
+        play2.update2()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(play1, ball) or sprite.collide_rect(play2, ball):
+            speed_x *= -1
+            speed_y *= 1
+            count_ball += 1
+
+        if ball.rect.y > win_height - 40 or ball.rect.y <= 0:
+            speed_y *= -1
+        
+        if ball.rect.x <= 0:
+            finish = True
+        if ball.rect.x >= win_width - 40:
+            finish = True
 
     display.update()  
     clock.tick(FPS)
